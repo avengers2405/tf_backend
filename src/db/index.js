4e03c31ea22e6feae2d09941ec0e43da8a0e8bd2
avengers2405 <akshitmishra24@gmail.com>
2026-02-21 15:16:17 +0530

    bug fies

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 import prisma from './prisma.js';
 
-res = await prisma.user.findMany({ take: 5 });
+const res = await prisma.user.findMany({ take: 5 });
 console.log("[DEBUG]: Users in PostgreSQL:", JSON.stringify(res), "\n\n");
 
 export { prisma };

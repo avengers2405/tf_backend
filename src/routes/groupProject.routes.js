4a0eec1f6001ca8c8c31ce2fdaece44502acc6d3
Neeraj <nneeraz306@gmail.com>
2026-03-24 23:42:09 +0530

    students can now see their academic project application status

--- src/routes/groupProject.routes.js
+++ src/routes/groupProject.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {  applyProjectTeam , getGroupsByProject, updateGroupApplicationStatus} from "../controller/groupProject.controller.js";
+import {  applyProjectTeam , getGroupsByProject, updateGroupApplicationStatus , getGroupStatusByProject} from "../controller/groupProject.controller.js";
  
 const router = Router();
 
 router.post("/apply", applyProjectTeam);
 router.get("/:project_id/groups" , getGroupsByProject)
 router.put("/:project_id/groups/:group_id/status",updateGroupApplicationStatus)
+router.get("/:project_id/my-group-status", getGroupStatusByProject);
 
  
 
